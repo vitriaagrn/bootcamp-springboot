348f6c3ce10da0291d6787edc744f1a2da3423a0
dimasm93 <software.dimas_m@icloud.com>
2022-09-06 10:49:35 +0700

    devops springboot

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/example")
+public class TestController {
+
+    @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test(){
+        return "Hai nama saya Dimas maryanto";
+    }
+
+}
